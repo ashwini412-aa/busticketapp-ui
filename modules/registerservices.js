71f2b5bda264826806dcb6a7d80c221496509008
ParasJalhotra1 <72965449+ParasJalhotra1@users.noreply.github.com>
2020-11-10 20:09:58 +0530

    updated services

--- modules/registerservices.js
+++ modules/registerservices.js
@@ -7,4 +7,19 @@ class RegisterService{
        let result="success";
        return result;
         }
+        getAllUsers() {
+            let users = JSON.parse(localStorage.getItem("USERS")) || [];
+            return users;
+        }
+        isEmailExists(email) {
+            let users = this.getAllUsers();
+            let exists = false;
+            for (let obj of users) {
+                if (obj.email == email) {
+                    exists = true;
+                    break;
+                }
+            }
+            return exists;
+        }
 }
